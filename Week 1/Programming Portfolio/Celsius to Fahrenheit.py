3200b69dd4f5195adea2bf0cabf1cbe2264295c9
Alice Woodard <a.woodard2386@student.leedsbeckett.ac.uk>
2023-10-19 11:10:09 +0100

    Week 3 Commit

--- Week 1/Programming Portfolio/Celsius to Fahrenheit.py
+++
@@ -1,4 +0,0 @@
-# Over the summer, temperatures in Yorkshire reached 38.4C.
-# Write a program that converts this value in Celsius to the equivalent temperature in Fahrenheit,
-# and then displays both.
-print("38.4C is "+ str((38.4 * 1.8) + 32) + "F")
